533e6f098328c215e3baa547aa53f49c39e1d50c
Manoj Gamachchige <manojpriyankaraucsc@gmail.com>
2020-11-08 11:19:44 +0800

    password encryption added

--- server/v1/db/user.query.js
+++ server/v1/db/user.query.js
@@ -1,7 +1,7 @@
 import knex from "server/util/knex";
 
 const signUpQuery = (data) => {
-  return knex("user").insert({ ...data });
+  return knex("user").insert(data, "*");
 };
 
 export default {
--- server/v1/services/authentication.service.js
+++ server/v1/services/authentication.service.js
@@ -1,18 +1,22 @@
 "use strict";
-import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import { v4 as uuidv4 } from "uuid";
+import bcrypt from "bcryptjs";
 
 import UserQuery from "server/v1/db/user.query";
 
 const signUp = async (userName, fullName, email, password, isMerchant) => {
+  const id = uuidv4();
   const addedUser = await UserQuery.signUpQuery({
+    id,
     user_name: userName,
     full_name: fullName,
     email,
-    password,
+    password: bcrypt.hashSync(password, 10),
     is_merchant: isMerchant,
+    status: true,
   });
-  return addedUser;
+  return addedUser && addedUser[0];
 };
 
 export default {
